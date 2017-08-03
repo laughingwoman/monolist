2d1e749ea42c387aaeb4b32a9ef1a0991cf37afb
taisei <ez.web.yahoo.com@gmail.com>
2017-08-03 09:12:38 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
 
       results.each do |result|
         # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
@@ -36,3 +36,12 @@ class ItemsController < ApplicationController
     }
   end
 end
+
+
+
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+  
+  
